f63caca2f1a11c0f08c7e6061b1a2a1405a91301
Hagerdarwish <hagardarwish.s@gmail.com>
2024-02-26 15:04:39 +0200

    Add fragment_news.xml to main activity_home.xml Add source items at the fragment_news.xml. Add error_view.xml and include it in fragment_news.xml.

--- app/src/main/java/com/example/newsapp/ui/Fragments/NewsFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/Fragments/NewsFragment.kt
@@ -5,11 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.newsapp.Api.ApiManager
+import com.example.newsapp.Api.SourceResponse
+import com.example.newsapp.Api.SourcesItem
 import com.example.newsapp.R
+import com.example.newsapp.databinding.FragmentNewsBinding
+import com.google.gson.Gson
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class NewsFragment : Fragment() {
-
+lateinit var binding: FragmentNewsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +28,51 @@ class NewsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_news, container, false)
+binding=FragmentNewsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    
-}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loadData()
+    }
+
+    private fun loadData() {
+        ApiManager.webServices().getSources("79f2d8fade644acebc6560c85b8914e2")
+            .enqueue(object : Callback<SourceResponse> {
+                override fun onResponse(
+                    call: Call<SourceResponse>,
+                    response: Response<SourceResponse>
+                ) {
+                    if (response.isSuccessful){
+                        response.body()?.sources.let {
+
+                            showTabs(it!!)
+                        }
+
+                    }
+                    else{
+
+                        Gson().fromJson(response.errorBody()?.string(),SourceResponse::class.java)
+                    }
+
+                }
+
+                override fun onFailure(call: Call<SourceResponse>, t: Throwable) {
+
+
+                }
+            })
+    }
+
+    private fun showTabs(sources:List<SourcesItem?>) {
+ sources?.forEach{
+     var tab=binding.Tab.newTab()
+     tab.text=it?.name
+     binding.Tab.addTab(tab)
+ }
+    }
+
+    }
+
+
--- app/src/main/java/com/example/newsapp/ui/HomeActivity.kt
+++ app/src/main/java/com/example/newsapp/ui/HomeActivity.kt
@@ -3,10 +3,12 @@ package com.example.newsapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.newsapp.R
+import com.example.newsapp.ui.Fragments.NewsFragment
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home2)
+        setContentView(R.layout.activity_home)
+        supportFragmentManager.beginTransaction().replace(R.id.frameLayout,NewsFragment(),"").commit()
     }
 }
